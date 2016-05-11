8305eb39356f172e4e175cdeebe94472aa5cfd4d
Debarghya <debarghya88@gmail.com>
2016-05-12 02:23:58 +0530

    Try DB Connection

--- usersearch/usersearch-userpersistence/src/main/java/dar/osgi/usersearch/userpersistence/UserActivityImpl.java
+++ usersearch/usersearch-userpersistence/src/main/java/dar/osgi/usersearch/userpersistence/UserActivityImpl.java
@@ -1,6 +1,15 @@
 package dar.osgi.usersearch.userpersistence;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import dar.osgi.usersearch.dbconnection.DBConnection;
+
 public class UserActivityImpl implements UserActivity {
+	DBConnection connection;
+	Connection con;
+	PreparedStatement pst;
 
 	@Override
 	public boolean login(String userName, String password) {
@@ -9,7 +18,40 @@ public class UserActivityImpl implements UserActivity {
 
 	@Override
 	public boolean register(String username, String password) {
+		String str="Insert into osgi_user(id,user_name,password) values(?,?,?) ";
+		con = connection.createConnection();
+		try {
+			pst=con.prepareStatement(str);
+			pst.setString(3,password);
+			pst.setString(2,username);
+			pst.setDouble(1,Math.random());
+			pst.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
+
+	public Connection getCon() {
+		return con;
+	}
+
+	public void setCon(Connection con) {
+		this.con = con;
+	}
+
+	public PreparedStatement getPst() {
+		return pst;
+	}
+
+	public void setPst(PreparedStatement pst) {
+		this.pst = pst;
+	}
+
+	public void setConnection(DBConnection connection) {
+		this.connection = connection;
+	}	
 
 }
--- usersearch/usersearch-userservice/src/main/java/dar/osgi/usersearch/userservice/UserRegisterImpl.java
+++ usersearch/usersearch-userservice/src/main/java/dar/osgi/usersearch/userservice/UserRegisterImpl.java
@@ -1,11 +1,18 @@
 package dar.osgi.usersearch.userservice;
 
+import dar.osgi.usersearch.userpersistence.UserActivity;
+
 public class UserRegisterImpl implements UserRegister {
+	
+	UserActivity userActivity;
 
 	@Override
 	public boolean register(String userName, String password) {
-		if(userName.equalsIgnoreCase("Deba")) return true;
-		return false;
+		return userActivity.register(userName, userName);
 	}
+
+	public void setUserActivity(UserActivity userActivity) {
+		this.userActivity = userActivity;
+	}	
 
 }
